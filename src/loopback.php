b16cb267249c2d3c5211d2cb79b9cfdd17feb3e1
Boone N <boone.neligan@gmail.com>
2015-05-03 11:41:59 -0400

    set Content-Type header on loopback.php to application/json for cleaner/leaner output

--- src/loopback.php
+++ src/loopback.php
@@ -1,30 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>loopback.php</title>
-</head>
-<body>
-    <?php
-    $reqArray;
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $reqArray = array('Type' => 'GET', 'parameters' => $_GET);
-        if (count($_GET) == 0) {
-            $reqArray['parameters'] = null;
-        }
-    }
-    else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $reqArray = array('Type' => 'POST', 'parameters' => $_POST);
-        if (count($_POST) == 0) {
-            $reqArray['parameters'] = null;
-        }
-    }
-    else {
-        die('<p>Invalid request.');
-    }
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+header('Content-Type: application/json');
 
-    echo json_encode($reqArray, JSON_FORCE_OBJECT);
+$reqArray;
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $reqArray = array('Type' => 'GET', 'parameters' => $_GET);
+    if (count($_GET) == 0) {
+        $reqArray['parameters'] = null;
+    }
+}
+else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $reqArray = array('Type' => 'POST', 'parameters' => $_POST);
+    if (count($_POST) == 0) {
+        $reqArray['parameters'] = null;
+    }
+}
+else {
+    die('<p>Invalid request.');
+}
 
-    ?>
-</body>
-</html>
+echo json_encode($reqArray, JSON_FORCE_OBJECT);
+
+?>
+
